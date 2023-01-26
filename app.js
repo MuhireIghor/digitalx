2f633603331e8c64faf13209d1dea707b67f7930
MuhireIghor <muhireighor123@gmail.com>
2023-01-26 07:56:53 +0200

    [feat]:auto suggest

--- app.js
+++ app.js
@@ -11,19 +11,20 @@ var client = new elasticsearch.Client({
   host: 'localhost:9200',
   log: 'trace'
 });
-var payload ={
+var payload = {
   "index": indexname,
-  "body": {  "settings": {
+  "body": {
+    "settings": {
       "analysis": {
         "analyzer": {
-           "indexing_analyzer": {
-             "tokenizer": "whitespace",
-             "filter":  ["lowercase", "edge_ngram_filter"]
-           },
-           "search_analyze": {
-             "tokenizer": "whitespace",
-             "filter":  "lowercase"
-           }
+          "indexing_analyzer": {
+            "tokenizer": "whitespace",
+            "filter": ["lowercase", "edge_ngram_filter"]
+          },
+          "search_analyze": {
+            "tokenizer": "whitespace",
+            "filter": "lowercase"
+          }
         },
         "filter": {
           "edge_ngram_filter": {
@@ -34,63 +35,85 @@ var payload ={
         }
       }
     },
-    "mappings":{
-      "world":{
-        "properties":{
+    "mappings": {
+      "world": {
+        "properties": {
           "title": {
             "type": "string",
-            "analyzer":"index_analyzer",
+            "analyzer": "index_analyzer",
             "search_analyzer": "search_analyze"
           },
           "tags": {
             "type": "string",
-            "analyzer":"indexing_analyzer",
+            "analyzer": "indexing_analyzer",
             "search_analyzer": "search_analyze"
           },
           "published": {
             "type": "string",
-            "analyzer":"indexing_analyzer",
+            "analyzer": "indexing_analyzer",
             "search_analyzer": "search_analyze"
           },
           "slug_de": {
             "type": "string",
-            "analyzer":"indexing_analyzer",
+            "analyzer": "indexing_analyzer",
             "search_analyzer": "search_analyze"
           },
           "slug_en": {
             "type": "string",
-            "analyzer":"indexing_analyzer",
+            "analyzer": "indexing_analyzer",
             "search_analyzer": "search_analyze"
           },
           "slug_de": {
             "type": "string",
-            "analyzer":"indexing_analyzer",
+            "analyzer": "indexing_analyzer",
             "search_analyzer": "search_analyze"
           },
           "introtext": {
             "type": "string",
-            "analyzer":"indexing_analyzer",
+            "analyzer": "indexing_analyzer",
             "search_analyzer": "search_analyze"
           },
         }
       }
-    }}
+    },
+    suggest: {
+      type: "completion",
+      analyzer: "simple",
+      search_analyzer: "simple",
+      payloads: true
+    }
+  }
 
 
 
 }
+function getSuggestion(input){
+return client.suggest({
+  index:indexname,
+  type:"document",
+  body:{
+    docsuggest:{
+      title:input,
+      completion:{
+        field:"suggest",
+        fuzzy:true
+      }
+    }
+  }
+})
+}
 
 
 
-app.get("/create",function(request, reponse){
-  client.indices.create(payload, function(error, response){
+app.get("/create", function (request, reponse) {
+  client.indices.create(payload, function (error, response) {
     getData();
   });
 });
 
 
 
-app.get("/search", function(req, resp){
+app.get("/search", function (req, resp) {
   console.log(req.query.search);
   client.search({
     index: indexname,
@@ -98,14 +121,17 @@ app.get("/search", function(req, resp){
       query: {
         multi_match: {
           query: req.query.search,
-          fields: ["title", "tags", "published","slug_de","slug_en","introtext"]
+          fields: ["title", "tags", "published", "slug_de", "slug_en", "introtext"]
         }
       }
     }
   }, function (error, response) {
-    resp.json({result: response});
+    resp.json({ result: response });
   });
 });
+app.get('/suggest/:input',function (req,res,next){
+getSuggestion(req.params.input).then((result)=>{res.json(result)})
+})
 
 app.listen("8080");
 
@@ -116,18 +142,18 @@ app.listen("8080");
 
 function getData() {
 
-  var data_array =[];
-  
-  for(var i = 0; i < data.length; i++){
+  var data_array = [];
+
+  for (var i = 0; i < data.length; i++) {
     data_array.push(
-      { index:  { _index: indexname, _type: 'world', _id: data[i].id } },
-      { title: data[i].title, tags: data[i].tags,published:data[i].published, slug_en: data[i].slug_en,slug_de:data[i].slug_de,introtext:data[i].introtext }
+      { index: { _index: indexname, _type: 'world', _id: data[i].id } },
+      { title: data[i].title, tags: data[i].tags, published: data[i].published, slug_en: data[i].slug_en, slug_de: data[i].slug_de, introtext: data[i].introtext }
     )
   }
   client.bulk({
     body: data_array
-  },function(error, response){
+  }, function (error, response) {
     console.log(error);
     console.log(response);
   });
-  };
+};
